9e4e6c04408744e9c76ada4c0e75bebdbca76bd8
GradyCongdon <gradycongdon@gmail.com>
2019-05-13 22:54:39 -0400

    init 1,2,3

---
+++ 2_1.js
@@ -0,0 +1,301 @@
+const d1 = `qwubbihrkplymcraxefntvdzns
+qwugbihrkplyzcjahefttvdzns
+qwugbihrkplymcjoxrsotvdzns
+qwugbphrkplympjaxmfotvdzns
+qwugbghrkpltbcjaxefotvdzns
+qwubbihrwpuymcjaxefotvdzns
+qiugpihrkplymcwaxefotvdzns
+qwugbihrkplymcjavefotvotns
+qwugbihrkpvymcjaxefotvnzes
+qwvgbihrkpltmcnaxefotvdzns
+qwugvihrkplymvjaxefotvczns
+qwugbihrkplymwjazefotvyzns
+qwugbihrkplbmcjbxefttvdzns
+qhugbihrkplymcjaxefotnazns
+qwugbihrkpyyacjacefotvdzns
+qwugsihrkpsymcjaxafotvdzns
+qwugbihriplymcjixefosvdzns
+qwuibihrkjlvmcjaxefotvdzns
+qwugbjhrgplymcjaxefotvdzys
+qwugnimrkplymcjayefotvdzns
+qwumjihrkplymcjexefotvdzns
+qwugbihukptymcjaxefotvvzns
+qwughthrgplymcjaxefotvdzns
+qlugbihrkplymcjbxhfotvdzns
+qhugbiyrkplymcjaxefotvdzes
+qwugbihrkvlymcjaxecotvtzns
+qwugbihrkphymcjaxefitvizns
+qwugbbhdtplymcjaxefotvdzns
+qwugbihrkplymceaxefotvltns
+mwugbihrkptymcpaxefotvdzns
+qwugbihrdplymcvaxefotvdwns
+qwugbihrkplymcjaxekhtvhzns
+qjugbihrkplyjcjaxefonvdzns
+qwugbihrjplymcjaxefgtudzns
+qlugbihrkplymcjaxefztvdpns
+qwugbihrkclyvpjaxefotvdzns
+qwugbihrsplymnjhxefotvdzns
+qwudbihrbxlymcjaxefotvdzns
+qwugbihrkplymcjxxefatvdzng
+qwujbihrkplyqdjaxefotvdzns
+qwugnihrkplamcjaxefotvmzns
+qwugnihrkplymcjajekotvdzns
+qwugbihrkslymcjamsfotvdzns
+fwugbihrkplymcjaxetotvdzne
+qwughihrkplyucfaxefotvdzns
+qwuebihrkplymcraxefotvdzgs
+qwugbinrkplymcjaxefodvdznh
+qwudbihrkplymcjsxefotvjzns
+qwlgbihrkzlymcjixefotvdzns
+qwugbihckpoymcpaxefotvdzns
+qwfgbibskplymcjaxefotvdzns
+qwugbihrkplymczaxdfotvuzns
+qwugbihwkplymcjaxepxtvdzns
+qwubbihrkplymcjaxefntfdzns
+qwunbihrkpaymcjaxefotvdzni
+qwugfihrkplymujaxefotvdzni
+qwugrihrkplymkjaxxfotvdzns
+ztugbihrkplymcjaxefotvdznt
+qwugbihrkplvmcjaxefotvdzph
+qwugtinrfplymcjaxefotvdzns
+qwugbihrkplamcjkmefotvdzns
+qwtgbihryplymcjaxeeotvdzns
+qwugbiazkplymhjaxefotvdzns
+pwugbihrkplymklaxefotvdzns
+wwugbihkxplymcjaxefotvdzns
+dwugbihrgpdymcjaxefotvdzns
+qwulbihrkplymcjaxefoqvqzns
+qvugbihrkplyhcjtxefotvdzns
+qwugbihrkplymcjaxcfotvfzjs
+qwugbihrkpkymyjaxdfotvdzns
+qwugbinrkplymcjswefotvdzns
+qcuguiqrkplymcjaxefotvdzns
+qwugbihlkplyccjaxefrtvdzns
+qwugbihpkplomcjaxefotvdhns
+qwggbphrkplymcjaxbfotvdzns
+qwuipihrkplymcjaxefotvdznt
+qwugbihrhplyccjaxeforvdzns
+qwugbdhrkplymcjdxefotvdznv
+qwugbihrkplymcjaxefotvbfos
+qwugtihrkplymcocxefotvdzns
+qwugbihrkpljmcjaxwfovvdzns
+qwugbnhrkplymcjaxefotvdxnm
+qwugbihrkpeymcjauefotvlzns
+qwugbihjkplymcraxefftvdzns
+qwugbghrkplymcjaxefotvizni
+qwwgbihrkplymcjrxefotvrzns
+qwugbihrkplymzjawexotvdzns
+qwugnihrkplymcjpxnfotvdzns
+qwugbihrkdlytcjaxecotvdzns
+qwugbihrkacymdjaxefotvdzns
+qlugbehrkplymcjaxzfotvdzns
+fwugbihrkplymcjamefotldzns
+qwugbihrkplymcjarefotlszns
+qwsgbihrkpnymcjfxefotvdzns
+awuubihrkplymcjaxefrtvdzns
+qwngbihrkpjtmcjaxefotvdzns
+qwugbihrkpltmkjaxefytvdzns
+ewugbihrkplymcjvxefotvdzus
+qwugbihrpplymcjaxsfmtvdzns
+qwrgbihrmplymcjaxefutvdzns
+qwugbihrknxymcwaxefotvdzns
+qwugbnurkplymcjabefotvdzns
+qwugbihrkphomfjaxefotvdzns
+qwugbchrkplymcjaxefctvdens
+qwugbidrkplymcjaxefotwwzns
+qwggbohrkplgmcjaxefotvdzns
+nwkgbihrkplymcjaxqfotvdzns
+qwuibihrkpnymajaxefotvdzns
+qwugsihrzplymujaxefotvdzns
+qwugbihrkplumcgaxefodvdzns
+qwugbqhrkplymcjaxefotvddts
+qwugbiorkpvyacjaxefotvdzns
+bjugbihukplymcjaxefotvdzns
+qwugbyhrkplymxjaxexotvdzns
+vwugbihrkplymcraxefotgdzns
+qwugbihrkplymjwaxeaotvdzns
+qwpsbihrkplykcjaxefotvdzns
+qwugbqhrkplymcjaxefotgdzno
+qwugbjhrkplymcjaxefatvczns
+qwuglihrkclymcjvxefotvdzns
+qjugbihrkpsymcjajefotvdzns
+qwugbinrkptymcjaxedotvdzns
+qwurbihrkglymcjaxefomvdzns
+qfugbihrsxlymcjaxefotvdzns
+lwuggihrkplymcjaxefotvdzds
+qwugbihrkplymcjhxwfjtvdzns
+qwugbhjrkplymcjaxefotvdyns
+qwugbihrkplymcjoxefepvdzns
+awwgbihryplymcjaxefotvdzns
+qpugbihrkplymcjaxekorvdzns
+qwulbihrkplymcuapefotvdzns
+qwugbwhrkplymljaxefotvdrns
+qwugxihrkplymjjalefotvdzns
+qwugbmhrkplymcjyxefotvdnns
+qwugbihrkplymcjnxgfotsdzns
+qwygbihrkplsmczaxefotvdzns
+qwugbihrkplymqjaxefovgdzns
+qwuwbihrkplymcjaxefktvdznu
+qwugbihrkplyfcjaxeoowvdzns
+qwufbiyrkplymcjaxedotvdzns
+qwusbirrkplymcjaxefotvdlns
+qwurbihroplymtjaxefotvdzns
+qiugbihrkplymcjaxefvtvmzns
+qwugrihrkflymcjaxefotvdzls
+qwugbimrzplymcoaxefotvdzns
+qyugbiwrkplymcjasefotvdzns
+qwubbihrkpiymcjaxefotvdzws
+qwugbilrkplymjjgxefotvdzns
+qwugbihykplympjaxefotgdzns
+qwugmxhrkplymcjaxefotvdins
+qwfjbahrkplymcjaxefotvdzns
+owuzbihrkplymcbaxefotvdzns
+qwugbihrkilymcjaxefotsdzvs
+qwugbwhrkplymcpzxefotvdzns
+qwugbihrkplymcjlcefotvdjns
+kwugbihrkplymcjaxefotvhdns
+qwulbihrkplymcfwxefotvdzns
+qwxabihrkplyhcjaxefotvdzns
+qwugbihrzpoymcjaxefotqdzns
+qwugbihrknlymcjabefovvdzns
+qyugbihrkplymclaxefotvgzns
+qwugbxhrkpgymcjaxefotvdlns
+qwuplihrkplymcjaxefhtvdzns
+qwugbihruplymcjaxefotmdzps
+qwugkihrkplymcqtxefotvdzns
+qwugbihrkplymcjaxeyodvszns
+qwukbihrkplymojaxefotvczns
+nwugbihrkplymcjaxrfothdzns
+qwugbihrkklymcjaxqfotvdzcs
+qwugbihrkplemcjaxefotvufns
+qwugbihrkplymcjaxbfitvdzne
+qwugbizrkplymcjaxgfotvdhns
+qwulbihrxplymcjaxefolvdzns
+jwugbihckpoymcpaxefotvdzns
+qwugeihrkplymbjcxefotvdzns
+qwuxbihbkplymcjaxeuotvdzns
+qwugbshrkplyvcjlxefotvdzns
+qwugbimrjplymcjaxtfotvdzns
+qwugzikrkplymcjaxefxtvdzns
+qwugbihrkplymcjaxefftvdgnq
+qwugbihnkilymcjaxemotvdzns
+qfugbihrkplyfcjadefotvdzns
+qwugbihrkplymrsaxefwtvdzns
+qwugfihrkpsymckaxefotvdzns
+qwulbihrkplymyjaxefotvdkns
+quugbikrkpkymcjaxefotvdzns
+qwugbihfgplymdjaxefotvdzns
+qougbihrkplemcjaqefotvdzns
+qwugbihrkplemcjjxefotvdyns
+qfuhbikrkplymcjaxefotvdzns
+qhugbihrcplymcjaxefrtvdzns
+qwugbmhnkplymcjnxefotvdzns
+qwugbihrkplymmjaaefofvdzns
+qwugbihrtplykcjaxefoxvdzns
+qwugbihrkmvymcjaxefetvdzns
+qwugbfjrkplymcjaxefotadzns
+qwuibihrrplymcjaxefotvdznv
+qwcgbihrkjlymcjzxefotvdzns
+qwugbihrkplymcjuxefytvzzns
+qwkgwihrkllymcjaxefotvdzns
+qwugbihrkplymcpaxgfogvdzns
+qwuvbihrkplymcdaxefotvdmns
+qwtgbihrkplqmcjgxefotvdzns
+qwuglihrkplnmcjaxefptvdzns
+qbugbihrkplymcjawefotidzns
+qwegbihrvplymcjaxefqtvdzns
+qwugbihrgqlyncjaxefotvdzns
+qwugbihrpplymcjaxefotvdeqs
+qwugbihrkplypzjaxefbtvdzns
+qwugbihrkpkyncjanefotvdzns
+qwugbshrkplymcjaxefotfdzys
+qwugbihrkpymmcjaxefotzdzns
+qwugbphrkplymcjaxefotvdzru
+qyugbihrkplamcjjxefotvdzns
+qwugbihrmphymcjaxefotedzns
+qwuafihrkplymcjaxefozvdzns
+qwugwihrkplymcjaxwfotvdzws
+qwugbihrkzlymcjaxjfotvdznz
+uwugsihrkplypcjaxefotvdzns
+qwugbihrkplymcjaxefotudzur
+qwugbihrkplymcjoxefotfdzng
+qwugbihxkplymcjamebotvdzns
+qpugvihrkplymcjaxefotvdzhs
+qwugbihrkplyocgaxefotvdzss
+qwugbihrkplymcpaqekotvdzns
+qwunbihrkplymclaxefitvdzns
+qzugbihrkplsmcjaxebotvdzns
+qvugbihrkplymcjsxefotvmzns
+qwugbihrkprymcyaxkfotvdzns
+qwugbihukplymcjaxefotzlzns
+qwusbihrkplymcjaxwfotxdzns
+qwugbihrwplymcjaxefbtcdzns
+qwugbihrkplymcjpxefotkdons
+qwugbihrkhlymcjaxefohvwzns
+qwukbihrkplymxjaxefotvdzms
+qwugbiprkplsmcjaxefotvdznm
+qwugbqhrkplymcjawwfotvdzns
+qwugbihrkprymcjaxefotvdxnb
+qwugbihrkplymcjaxefoivpzos
+qwugbuhrkplymcjaxefotvdzsb
+qwugblhrcplymcjaxefotvdyns
+qtuabihrkplymejaxefotvdzns
+qwucbihrkplyvcjaxefotvdznu
+rwugbyhrkplymcjaxefotvdzrs
+qruybihrkpsymcjaxefotvdzns
+qwugbihrjpwymcjaxejotvdzns
+qwugbihshplymcjaxefoavdzns
+vwugbihrkplymwjaxefotvdznc
+qwugbihrkpmymcjvxcfotvdzns
+qkxgbihrkplymcjnxefotvdzns`.split("\n");
+
+const d2 = `abcdef
+bababc
+abbcde
+abcccd
+aabcdd
+abcdee
+ababab`.split('\n');
+const data = d1;
+
+let twos = 0;
+let threes = 0;
+
+for (let i = 0; i < data.length; i++) {
+  const d = data[i];
+  let two = 0;
+  let three = 0;
+  const letters = {};
+  for (let j = 0; j < d.length; j++) {
+    const c = d[j];
+    if (!letters[c]) {
+      letters[c] = 1;
+      continue;
+    }
+    letters[c] = letters[c] + 1;
+  }
+  for (let v of Object.values(letters)) {
+    if (v === 2) {
+      two = 1;
+    }
+    if (v === 3) {
+      three = 1;
+    }
+  }
+
+
+
+  if (two) {
+    twos += 1;
+  }
+  if (three) {
+    threes += 1;
+  }
+}
+
+
+console.log(data.length);
+console.log(twos,threes);
+console.log(twos * threes);
+
+
