3807948c4139f301ffa5ffe5b52e2fd664c73476
Grady Congdon <gradycongdon@gmail.com>
2019-11-04 13:18:31 -0500

    ugh

--- 11.p2.js
+++ 11.p2.js
@@ -1,6 +1,10 @@
-const debug = true;
-const out = (x) =>  debug && process.stdout.write(x);
-const write = (x) =>  debug && process.stdout.write(x);
+const logs = {
+  all: false,
+};
+
+const debug = (x,l = 'all') =>  logs['all'] && logs[l] && process.stdout.write(x);
+const write = (x) => process.stdout.write(x.toString());
+const print = (x) => process.stdout.write(x + ' ');
 
 const buildPower = (serial) => {
   const power = (x,y) => {
@@ -21,9 +25,9 @@ const buildLogWindow = (xMin, xMax, yMin, yMax) => {
   const width = xMax - xMin;
   const logWindow = (x,y,p) => {
     if ((x >= xMin && x <= xMax) && (y >= yMin && y <= yMax)) {
-      process.stdout.write(p.toString().padStart(3,' '));
+      process.stdout.print(p.toString().padStart(3,' '));
       const diff = x - xMin;
-      if (diff !== 0 && diff % width === 0) process.stdout.write('\n');
+      if (diff !== 0 && diff % width === 0) process.stdout.print('\n');
     }
   }
   return logWindow;
@@ -48,6 +52,27 @@ const graph = (x,y,p) => {
     return true;
 }
 
+const printer = (x,y,w) => {
+  const endX = w + x;
+  const endY = w + y;
+  let score = 0;
+  for (let yyy = y; yyy < endY; yyy++) {
+    for (let xxx = x; xxx < endX; xxx++) {
+      let p = grid[yyy][xxx];
+      score = score + p;
+      if (p >= 0) {
+        write(` ${p}`);
+      } else {
+        write(`${p}`);
+      }
+    }
+    write('\n');
+  }
+  write(`--- ${score} --- `);
+  write('\n');
+  write('\n');
+}
+
 function test() {
   const power8 = buildPower(8);
   const power57 = buildPower(57);
@@ -69,12 +94,12 @@ function test() {
 }
 
 
-const serial = 2568;
-const size = 300;
+const serial = 18;
+const size = 3;
 
 const power = buildPower(serial);
-const sizes = coords(3,1,33,45);
-const logWindow = buildLogWindow(...sizes);
+// const sizes = coords(3,1,33,45);
+// const logWindow = buildLogWindow(...sizes);
 
 const grid = new Array();
 grid.push(new Array({length: size}));
@@ -84,9 +109,10 @@ for (let y = 1; y <= size; y++) {
   for (let x = 0; x <= size; x++) {
     const powerLevel = power(x,y);
     col.push(powerLevel);
-    //graph(x,y,p,size);
+    write(powerLevel);
   }
   grid.push(col);
+  print('\n');
 }
 
 
@@ -95,64 +121,53 @@ let mX = 0;
 let mY = 0;
 let mW = 0;
 
-let w = 2;
-
-const printer = (w) => {
-  for (let yyy = 1; yyy <= w; yyy++) {
-    for (let xxx = 1; xxx <= w; xxx++) {
-      if (yyy !== w && xxx !== w) {
-        write(' . ');
-      } else {
-        let p = grid[yyy][xxx];
-        score = score + p
-        if (p >= 0) {
-          write(` ${p} `);
-        } else {
-          write(`${p} `);
-        }
-      }
-    }
-    write('\n');
-  }
-}
-
-y:
 for (let y = 1; y <= size; y++) {
   for (let x = 1; x <= size; x++) {
     let score = 0;
-    write(`\n (${x}, ${y}) `);
-    for (w = 1; w <= (size - y); w++) {
-      const next = w+1;
+    const big = Math.max(x,y);
+    console.log(`${x},${y}:`);
+    for (let w = 1; w <= (size - big); w++) {
+      console.log(`${w}x / ${size - big + 1}`);
+      const last = score;
+      let current = 0;
 
-      // calc border
-      // ..1
-      // ..2
-      // 123x2
-      for (let i = 1; i < w; i++) {
-        score = score + grid[next][i];
-        score = score + grid[i][next];
+      if (w == 1) {
+        current = grid[y][x];
+        print(grid[y][x]);
+      } else {
+        // calc border
+        // ..1
+        // ..2
+        // 123x2
+        const rows = [];
+        for (let i = 1; i <= w; i++) {
+          const col = grid[y][i];
+          const row = grid[i][x];
+          current = current + col
+          current = current + row
+          rows.push(row);
+          console.log(col);
+        }
+        console.log(rows.join(' ') + '\n\n');
+        current = current - grid[y][x]; // remove 1 of double corner
       }
+      score = score + current;
 
-      score = score - grid[next][next]; // remove 1 corner
-
-      if (score >= max) {
+      if (score > max) {
         max = score;
         mX = x;
         mY = y;
         mW = w;
+        //console.log(`${serial} : (${mX}, ${mY}, ${mW}) -> ${max}`);
       }
-      if (w % 10 === 0) write(`${w} -> ${score} `);
+      //if (w % 9 === 0) print(`(${x}, ${y}, ${w}) -> ${score} \n`);
     } // w
-    write('\n');
+    console.log();
+    //if (x > 2) return;
   } // x
 }
+return;
 
 console.log(`${serial} : (${mX}, ${mY}, ${mW}) -> ${max}`);
-
-
-
-
-  
-
-
-
+printer(mX,mY,mW);
+printer(33,45,3);
